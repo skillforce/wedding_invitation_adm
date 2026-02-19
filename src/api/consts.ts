cbbda7e47fc8a1acc913d9ac9a8c634b818c1aeb
denis_tatarinov <skillforce@mail.ru>
2026-02-19 16:28:21 +0300

    chore: update env var name to VITE_API_URL

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -6,7 +6,7 @@ export enum HttpMethod {
 }
 
 
-export const BASE_API_URL = import.meta.env.VITE_APP_API_URL ?? ''
+export const BASE_API_URL = import.meta.env.VITE_API_URL ?? ''
 
 export const BASE_REQUEST_CONFIG: RequestInit = {
   headers: {
