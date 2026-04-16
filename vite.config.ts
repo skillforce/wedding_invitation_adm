524180c8caee6a4c2e2cea3b5c33cde65f8c383a
denis_tatarinov <skillforce@mail.ru>
2026-04-16 16:23:50 +0300

    Generating commit message...

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ command }) => ({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
     },
   },
   build: {
