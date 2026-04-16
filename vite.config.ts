51bbd2e05b9fbdc361d26899f2fd73ba523f33e6
denis_tatarinov <skillforce@mail.ru>
2026-04-16 15:40:51 +0300

    feat(build): add vue-i18n to vendor-core chunk config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ command }) => ({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
     },
   },
   build: {
